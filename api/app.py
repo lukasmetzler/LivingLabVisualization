55ea72f068a23e7f402fc99d8a9ac1c60bffb430
MetzlerL <lukas.metzler@compax.at>
2023-11-09 11:25:33 +0100

    added first "GET" route for metrological_data and added the db cursor library

--- api/app.py
+++ api/app.py
@@ -1,7 +1,26 @@
 from flask import Flask, jsonify
+import psycopg2
+
+conn = psycopg2.connect(
+    database="EVI",
+    user="lukasmetzler",
+    password="lukasmetzler",
+    host="localhost",
+    port="5432"
+)
 
 app = Flask(__name__)
 
+@app.route('/api/metrological_data', methods=['GET'])
+def get_metrological_data():
+    cursor = conn.cursor()
+    cursor.execute('SELECT * FROM metrological_data')
+    data = cursor.fetchall()
+    cursor.close()
+    return jsonify(data)
+
+
+###############################################################
 @app.route('/api/data', methods=['GET'])
 def get_data():
     # Hier könntest du Logik hinzufügen, um Daten zu generieren
