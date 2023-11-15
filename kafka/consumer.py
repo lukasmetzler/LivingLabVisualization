c1ec139185c6fd49076e4d483828c8444caa6e8d
Lukas Metzler <lukas.metzler@wheele.de>
2023-11-15 22:19:42 +0100

    edited and now functional

--- kafka/consumer.py
+++ kafka/consumer.py
@@ -1,48 +1,84 @@
-from json import loads  
-from kafka import KafkaConsumer  
+from json import loads
+from kafka import KafkaConsumer
 import psycopg2
+import logging
+from time import sleep
+
+logging.basicConfig(
+    filename="consumer.log",
+    level=logging.INFO,
+    format="%(asctime)s - %(levelname)s - %(message)s",
+)
 
 db_params = {
-    'dbname': 'EVI',
-    'user': 'lukasmetzler',
-    'password': 'lukasmetzler',
-    'host': 'localhost',
-    'port': '5432'
+    "dbname": "evi",
+    "user": "lukasmetzler",
+    "password": "lukasmetzler",
+    "host": "localhost",
+    "port": "5432",
 }
 
 metrological_column_names = [
-    'GlobalIrrVerAct', 'GlobIrrVerAct', 'GlobalIrrHorAct', 'DifflrrHorAct',
-    'WindSpeedAct_ms', 'SunElevationAct', 'SunAzimuthAct', 'Longitude',
-    'Latitude', 'WindSpeedAct_kmh', 'WindDirectionAct', 'BrightnessNorthAct',
-    'BrightnessSouthAct', 'BrightnessWestAct', 'TwilightAct', 'GlobalIrrHorAct_2',
-    'PrecipitationAct', 'AbsolutAirPressureAct', 'RelativeAirPressureAct',
-    'AbsoluteHumidityAct', 'RelativeHumidityAct', 'DewPointTempAct', 'HousingTemAct',
-    'RoomTempAct'
+    "GlobalIrrVerAct",
+    "GlobIrrVerAct",
+    "GlobalIrrHorAct",
+    "DifflrrHorAct",
+    "WindSpeedAct_ms",
+    "SunElevationAct",
+    "SunAzimuthAct",
+    "Longitude",
+    "Latitude",
+    "WindSpeedAct_kmh",
+    "WindDirectionAct",
+    "BrightnessNorthAct",
+    "BrightnessSouthAct",
+    "BrightnessWestAct",
+    "TwilightAct",
+    "GlobalIrrHorAct_2",
+    "PrecipitationAct",
+    "AbsolutAirPressureAct",
+    "RelativeAirPressureAct",
+    "AbsoluteHumidityAct",
+    "RelativeHumidityAct",
+    "DewPointTempAct",
+    "HousingTemAct",
+    "RoomTempAct",
 ]
 
+
 consumer = KafkaConsumer(
-    'dim_metrological_data_topic', 
-    bootstrap_servers=['localhost:9092'],
-    auto_offset_reset='earliest',
+    "dim_metrological_data_topic",
+    bootstrap_servers=["localhost:9092"],
+    auto_offset_reset="earliest",
     enable_auto_commit=True,
-    group_id='my-group',
-    value_deserializer=lambda x: loads(x.decode('utf-8'))
+    group_id="my-group",
+    value_deserializer=lambda x: loads(x.decode("utf-8")),
 )
+
+sleep(10)
 
 conn = psycopg2.connect(**db_params)
 cursor = conn.cursor()
 
-for message in consumer:
-    metrological_data = message.value
-    
-    columns_placeholder = ', '.join(metrological_column_names)
-    
-    values_placeholder = ', '.join(['%s'] * len(metrological_column_names))
+try:
+    for message in consumer:
+        metrological_data = message.value
 
-    query = f"INSERT INTO dim_metrological_data ({columns_placeholder}) VALUES ({values_placeholder})"
-    
-    values = [metrological_data[column] for column in metrological_column_names]
-    
-    cursor.execute(query, values)
+        columns_placeholder = ", ".join(metrological_column_names)
+        values_placeholder = ", ".join(["%s"] * len(metrological_column_names))
 
-    conn.commit()
+        query = f"INSERT INTO dim_metrological_data ({columns_placeholder}) VALUES ({values_placeholder})"
+        values = [metrological_data[column] for column in metrological_column_names]
+
+        cursor.execute(query, values)
+        conn.commit()
+
+        logging.info(f"Data inserted into database: {metrological_data}")
+
+except Exception as e:
+    logging.error(f"An error occurred: {e}")
+
+finally:
+    cursor.close()
+    conn.close()
+    logging.info("Database connection closed.")
--- kafka/producer.py
+++ kafka/producer.py
@@ -1,39 +1,42 @@
-from time import sleep  
-from json import dumps 
+from time import sleep
+from json import dumps
 from kafka import KafkaProducer
 
+
+sleep(10)
 producer = KafkaProducer(
-    bootstrap_servers=['localhost:9092'],
-    value_serializer=lambda x: dumps(x).encode('utf-8')
+    bootstrap_servers=["localhost:9092"],
+    value_serializer=lambda x: dumps(x).encode("utf-8"),
 )
 
 
 while True:
     sleep(60)
     metrological_data = {
-        'GlobalIrrVerAct': 121,
-        'GlobIrrVerAct': 121,
-        'GlobalIrrHorAct': 121,
-        'DifflrrHorAct': 121,
-        'WindSpeedAct_ms': 121,
-        'SunElevationAct': 121,
-        'SunAzimuthAct': 121,
-        'Longitude': 121,
-        'Latitude': 121,
-        'WindSpeedAct_kmh': 121,
-        'WindDirectionAct': 121,
-        'BrightnessNorthAct': 121,
-        'BrightnessSouthAct': 121,
-        'BrightnessWestAct': 121,
-        'TwilightAct': 121,
-        'GlobalIrrHorAct_2': 121,
-        'PrecipitationAct': 121,
-        'AbsolutAirPressureAct': 121,
-        'RelativeAirPressureAct': 121,
-        'AbsoluteHumidityAct': 121,
-        'RelativeHumidityAct': 121,
-        'DewPointTempAct': 121,
-        'HousingTemAct': 121,
-        'RoomTempAct': 121
+        "GlobalIrrVerAct": 121,
+        "GlobIrrVerAct": 121,
+        "GlobalIrrHorAct": 121,
+        "DifflrrHorAct": 121,
+        "WindSpeedAct_ms": 121,
+        "SunElevationAct": 121,
+        "SunAzimuthAct": 121,
+        "Longitude": 121,
+        "Latitude": 121,
+        "WindSpeedAct_kmh": 121,
+        "WindDirectionAct": 121,
+        "BrightnessNorthAct": 121,
+        "BrightnessSouthAct": 121,
+        "BrightnessWestAct": 121,
+        "TwilightAct": 121,
+        "GlobalIrrHorAct_2": 121,
+        "PrecipitationAct": 121,
+        "AbsolutAirPressureAct": 121,
+        "RelativeAirPressureAct": 121,
+        "AbsoluteHumidityAct": 121,
+        "RelativeHumidityAct": 121,
+        "DewPointTempAct": 121,
+        "HousingTemAct": 121,
+        "RoomTempAct": 121,
     }
-    producer.send('dim_metrological_data_topic', value=metrological_data)
+    producer.send("dim_metrological_data_topic", value=metrological_data)
+    sleep(0.5)
