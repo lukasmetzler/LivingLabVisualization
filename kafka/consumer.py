6cca879f29d65edcbc919b9d023c4af6513f2be2
Lukas Metzler <lukas.metzler@wheele.de>
2023-11-30 19:16:39 +0100

    consumer finally works!

--- kafka/consumer.py
+++ kafka/consumer.py
@@ -5,7 +5,7 @@ import logging
 import config
 import postgres as pg
 
-logging.basicConfig(level=logging.INFO)
+# logging.basicConfig(level=logging.DEBUG)
 
 c = config.load_config()
 logging.info("Creating KafkaConsumer...")
@@ -13,7 +13,7 @@ consumer = KafkaConsumer(
     c.KAFKA_TOPIC,
     bootstrap_servers=[c.KAFKA_BOOTSTRAP_SERVER],
     auto_offset_reset="earliest",
-    enable_auto_commit=True,
+    enable_auto_commit=False,
     group_id="consumer",
     value_deserializer=lambda x: json.loads(x.decode("utf-8")),
 )
@@ -45,63 +45,73 @@ metrological_column_names = [
     "roomtempact",
 ]
 
-with pg.postgres_cursor_context() as cursor:
+with pg.postgres_connection() as connection:
     try:
-        cursor.execute(
+        with connection.cursor() as cursor:
+            cursor.execute(
+                """
+                CREATE TABLE IF NOT EXISTS dim_metrological_data (
+                    metrological_data_id SERIAL PRIMARY KEY,
+                    globirrveract Numeric,
+                    globalirrhoract Numeric,
+                    difflrrhoract Numeric,
+                    windspeedact_ms Numeric,
+                    sunelevationact Numeric,
+                    sunazimuthact Numeric,
+                    longitude Numeric,
+                    latitude Numeric,
+                    windspeedact_kmh Numeric,
+                    winddirectionact Numeric,
+                    brightnessnorthact Numeric,
+                    brightnesssouthact Numeric,
+                    brightnesswestact Numeric,
+                    twilightact Numeric,
+                    globalirrhoract_2 Numeric,
+                    precipitationact Numeric,
+                    absolutairpressureact Numeric,
+                    relativeairpressureact Numeric,
+                    absolutehumidityact Numeric,
+                    relativehumidityact Numeric,
+                    dewpointtempact Numeric,
+                    housingtemact Numeric,
+                    roomtempact Numeric
+                );
             """
-            CREATE TABLE IF NOT EXISTS dim_metrological_data (
-                metrological_data_id SERIAL PRIMARY KEY,
-                globirrveract Numeric,
-                globalirrhoract Numeric,
-                difflrrhoract Numeric,
-                windspeedact_ms Numeric,
-                sunelevationact Numeric,
-                sunazimuthact Numeric,
-                longitude Numeric,
-                latitude Numeric,
-                windspeedact_kmh Numeric,
-                winddirectionact Numeric,
-                brightnessnorthact Numeric,
-                brightnesssouthact Numeric,
-                brightnesswestact Numeric,
-                twilightact Numeric,
-                globalirrhoract_2 Numeric,
-                precipitationact Numeric,
-                absolutairpressureact Numeric,
-                relativeairpressureact Numeric,
-                absolutehumidityact Numeric,
-                relativehumidityact Numeric,
-                dewpointtempact Numeric,
-                housingtemact Numeric,
-                roomtempact Numeric
-            );
-        """
-        )
-        for message in consumer:
-            metrological_data = message.value
+            )
 
-            # Überprüfen, ob alle erforderlichen Schlüssel vorhanden sind
-            if all(key in metrological_data for key in metrological_column_names):
-                values = [
-                    metrological_data[column] for column in metrological_column_names
-                ]
+            for message in consumer:
+                print("Received message:", message.value)
+                metrological_data = message.value
 
-                columns_placeholder = ", ".join(metrological_column_names)
-                values_placeholder = ", ".join(["%s"] * len(metrological_column_names))
+                if all(key in metrological_data for key in metrological_column_names):
+                    values = [
+                        metrological_data[column]
+                        for column in metrological_column_names
+                    ]
 
-                query = f"INSERT INTO dim_metrological_data ({columns_placeholder}) VALUES ({values_placeholder})"
-                print("QUERY: " + query)
-                print("VALUES TEST: ", values)
+                    columns_placeholder = ", ".join(metrological_column_names)
+                    values_placeholder = ", ".join(
+                        ["%s"] * len(metrological_column_names)
+                    )
 
-                try:
-                    cursor.execute(query, values)
-                    logging.info(f"Data inserted into database: {metrological_data}")
-                except Exception as e:
-                    logging.error(f"An error occurred: {e}")
-            else:
-                logging.error(
-                    "Not all required keys present in metrological_data. Skipping message."
-                )
+                    query = f"INSERT INTO dim_metrological_data ({columns_placeholder}) VALUES ({values_placeholder})"
+                    print("QUERY: " + query)
+                    print("VALUES TEST: ", values)
+
+                    try:
+                        cursor.execute(query, values)
+                        connection.commit()
+                        logging.info(
+                            f"Data inserted into database: {metrological_data}"
+                        )
+                        print("Data inserted into database:", metrological_data)
+                    except Exception as e:
+                        logging.error(f"An error occurred: {e}")
+                        print("Error inserting data into database:", e)
+                else:
+                    logging.error(
+                        "Not all required keys present in metrological_data. Skipping message."
+                    )
 
     except Exception as e:
         logging.error(f"An error occurred: {e}")
--- kafka/postgres.py
+++ kafka/postgres.py
@@ -2,17 +2,18 @@ import contextlib
 import psycopg2
 import psycopg2.extensions
 import config
+import logging
 
 
 @contextlib.contextmanager
-def postgres_cursor_context(
+def postgres_connection(
     user: str = None,
     password: str = None,
     host: str = None,
     port: int = None,
     database: str = None,
-) -> "psycopg2.extensions.cursor":
-    """Creates a context with a psycopg2 cursor for a database alias"""
+) -> "psycopg2.extensions.connection":
+    """Creates a context with a psycopg2 connection for a database alias"""
     c = config.load_config()
     user = user if user is not None else c.CONSUMER_POSTGRES_USER
     password = password if password is not None else c.CONSUMER_POSTGRES_PASSWORD
@@ -23,13 +24,10 @@ def postgres_cursor_context(
     connection = psycopg2.connect(
         dbname=database, user=user, password=password, host=host, port=port
     )
-    cursor = connection.cursor()
+    logging.info("Connected to PostgreSQL database.")
     try:
-        yield cursor
-        connection.commit()
+        yield connection
     except Exception as e:
-        connection.rollback()
         raise e
     finally:
-        cursor.close()
         connection.close()
