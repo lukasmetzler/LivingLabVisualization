4499d16e41a82b151c2761ca30bec551cd3fc117
Lukas Metzler <lukas.metzler@wheele.de>
2023-12-08 14:30:28 +0100

    producer edit for all tables and more general

--- kafka/producer.py
+++ kafka/producer.py
@@ -4,8 +4,13 @@ from kafka import KafkaProducer
 import config
 import logging
 import random
+from typing import Dict, List
+import column_names as cn
 
-logging.basicConfig(level=logging.DEBUG)
+
+def generate_random_data(column_names: List[str]) -> Dict[str, float]:
+    return {column: random.uniform(0, 100) for column in column_names}
+
 
 c = config.load_config()
 print("Configuration loaded.")
@@ -21,36 +26,7 @@ wait_between_iterations = c.PRODUCER_INTERVAL_SECONDS
 print("Starting producer loop...")
 
 while True:
-    metrological_data = {
-        "globirrveract": random.uniform(100, 200),
-        "globalirrhoract": random.uniform(100, 200),
-        "difflrrhoract": random.uniform(100, 200),
-        "windspeedact_ms": random.uniform(0, 10),
-        "sunelevationact": random.uniform(0, 90),
-        "sunazimuthact": random.uniform(0, 360),
-        "longitude": random.uniform(-180, 180),
-        "latitude": random.uniform(-90, 90),
-        "windspeedact_kmh": random.uniform(0, 36),
-        "winddirectionact": random.uniform(0, 360),
-        "brightnessnorthact": random.uniform(0, 100),
-        "brightnesssouthact": random.uniform(0, 100),
-        "brightnesswestact": random.uniform(0, 100),
-        "twilightact": random.uniform(0, 1),
-        "globalirrhoract_2": random.uniform(100, 200),
-        "precipitationact": random.uniform(0, 10),
-        "absolutairpressureact": random.uniform(900, 1100),
-        "relativeairpressureact": random.uniform(0, 100),
-        "absolutehumidityact": random.uniform(0, 100),
-        "relativehumidityact": random.uniform(0, 100),
-        "dewpointtempact": random.uniform(-20, 40),
-        "housingtemact": random.uniform(-20, 40),
-        "roomtempact": random.uniform(-20, 40),
-    }
-    dim_pv_modul_data_1og_r1_data = {
-        "volt_meas_act_module1": random.uniform(100, 200),
-        "curr_meas_act_module1": random.uniform(100, 200),
-        "volt_meas_act_module2": random.uniform(100, 200),
-        "curr_meas_act_module2": random.uniform(100, 200),
-    }
-    producer.send(kafka_topic, value=metrological_data)
+    for table_name in cn.table_column_names:
+        data_for_table = generate_random_data(table_name)
+        producer.send(kafka_topic, value={table_name: data_for_table})
     sleep(wait_between_iterations)
