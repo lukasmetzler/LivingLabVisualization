c688e617233ce1da17e24a3d10b684a223295258
Lukas Metzler <lukas.metzler@wheele.de>
2024-02-27 21:31:18 +0100

    Refactoring and addition of more clear topic

--- kafka/producer.py
+++ kafka/producer.py
@@ -1,17 +1,20 @@
-from time import sleep
 import json
-from kafka import KafkaProducer
-import config
 import logging
 import random
-from typing import Dict, List
 import signal
 import sys
+from time import sleep
+from typing import Dict, List
 import psycopg2
+from kafka import KafkaProducer
+import config
 import column_names as cn
 
 
 def stop_producer(signum, frame):
+    """
+    Stop producer when SIGINT signal received.
+    """
     logging.info("Stopping producer...")
     producer.close()
     db_connection.close()
@@ -19,46 +22,65 @@ def stop_producer(signum, frame):
 
 
 def generate_random_data(table_column_names: Dict[str, List[str]]) -> Dict[str, float]:
+    """
+    Generate random data for tables based on column names.
+    """
     data = {}
-
     for table, columns in table_column_names.items():
-        # Exclude columns ending with '_id' (assuming these are the ID columns)
-        filtered_columns = [column for column in columns if not column.endswith('_id')]
-        
-        # Generate random data for each non-ID column
+        filtered_columns = [column for column in columns if not column.endswith("_id")]
         data[table] = {column: random.uniform(0, 100) for column in filtered_columns}
-
     logging.debug("Generated data: %s", data)
     return data
 
 
-c = config.load_config()
-print("Configuration loaded.")
-logging.info("Starting the Producer...")
-print(f"KAFKA_BOOTSTRAP_SERVER: {c.KAFKA_BOOTSTRAP_SERVER}")
+def establish_db_connection(configurations):
+    """
+    Establish PostgreSQL connection.
+    """
+    return psycopg2.connect(
+        dbname=configurations.CONSUMER_POSTGRES_DB,
+        user=configurations.CONSUMER_POSTGRES_USER,
+        password=configurations.CONSUMER_POSTGRES_PASSWORD,
+        host=configurations.CONSUMER_POSTGRES_HOST,
+        port=configurations.CONSUMER_POSTGRES_PORT,
+    )
 
-# Establish PostgreSQL connection
-db_connection = psycopg2.connect(
-    dbname=c.CONSUMER_POSTGRES_DB,
-    user=c.CONSUMER_POSTGRES_USER,
-    password=c.CONSUMER_POSTGRES_PASSWORD,
-    host=c.CONSUMER_POSTGRES_HOST,
-    port=c.CONSUMER_POSTGRES_PORT,
-)
 
-producer = KafkaProducer(
-    bootstrap_servers=[c.KAFKA_BOOTSTRAP_SERVER],
-    value_serializer=lambda v: json.dumps(v).encode("utf-8"),
-)
+def start_producer(configurations):
+    """
+    Start Kafka producer.
+    """
+    return KafkaProducer(
+        bootstrap_servers=[configurations.KAFKA_BOOTSTRAP_SERVER],
+        value_serializer=lambda v: json.dumps(v).encode("utf-8"),
+    )
 
-kafka_topic = c.KAFKA_TOPIC
-wait_between_iterations = c.PRODUCER_INTERVAL_SECONDS
-print("Starting the producer loop...")
 
-signal.signal(signal.SIGINT, stop_producer)
+def main():
+    """
+    Main function to start the producer loop.
+    """
+    configurations = config.load_config()
+    logging.info("Starting the Producer...")
+    logging.info(f"KAFKA_BOOTSTRAP_SERVER: {configurations.KAFKA_BOOTSTRAP_SERVER}")
 
-while True:
-    data_for_tables = generate_random_data(cn.table_column_names)
-    logging.debug("Data for tables: %s", data_for_tables)
-    producer.send(kafka_topic, value=data_for_tables)
-    sleep(wait_between_iterations)
+    global db_connection, producer
+    db_connection = establish_db_connection(configurations)
+    producer = start_producer(configurations)
+
+    kafka_topic = configurations.KAFKA_TOPIC
+    wait_between_iterations = configurations.PRODUCER_INTERVAL_SECONDS
+    logging.info("Starting the producer loop...")
+
+    signal.signal(signal.SIGINT, stop_producer)
+
+    while True:
+        data_for_tables = generate_random_data(cn.table_column_names)
+        logging.debug("Data for tables: %s", data_for_tables)
+        producer.send(kafka_topic, value=data_for_tables)
+        sleep(wait_between_iterations)
+
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO)
+    main()
