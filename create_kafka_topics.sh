8da37f3e9d46bc6f155b64b3e31459af3f0dcc2d
MetzlerL <lukas.metzler@compax.at>
2024-07-02 10:20:15 +0200

    create_kafka_topics skript

---
+++ create_kafka_topics.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Wait for Kafka to start
+sleep 10
+
+# Create Kafka topic
+kafka-topics.sh --create --topic hella_data_topic --bootstrap-server kafka_new:9092 --replication-factor 1 --partitions 1
