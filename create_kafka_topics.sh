eae29d731e3000e50e0e434ccb1394a11291e923
MetzlerL <lukas.metzler@compax.at>
2024-07-02 10:24:40 +0200

    create_kafka_topics

--- create_kafka_topics.sh
+++ create_kafka_topics.sh
@@ -4,4 +4,4 @@
 sleep 10
 
 # Create Kafka topic
-kafka-topics.sh --create --topic hella_data_topic --bootstrap-server kafka_new:9092 --replication-factor 1 --partitions 1
+/usr/bin/kafka-topics --create --topic hella_data_topic --bootstrap-server kafka_new:9092 --replication-factor 1 --partitions 1
