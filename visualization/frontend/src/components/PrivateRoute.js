1e9b27aedbbee0795c5a24d37b6ef425c7d46326
Lukas Metzler <lukas.metzler@wheele.de>
2024-07-04 22:12:53 +0200

    PrivateRoute.js

--- visualization/frontend/src/components/PrivateRoute.js
+++ visualization/frontend/src/components/PrivateRoute.js
@@ -1,19 +1,9 @@
 import React from "react";
-import { Route, Redirect } from "react-router-dom";
+import { Navigate, Outlet } from "react-router-dom";
 
-const PrivateRoute = ({ component: Component, ...rest }) => {
-  return (
-    <Route
-      {...rest}
-      render={(props) =>
-        localStorage.getItem("token") ? (
-          <Component {...props} />
-        ) : (
-          <Redirect to="/login" />
-        )
-      }
-    />
-  );
+const PrivateRoute = () => {
+  const isAuthenticated = !!localStorage.getItem("token");
+  return isAuthenticated ? <Outlet /> : <Navigate to="/login" />;
 };
 
 export default PrivateRoute;
