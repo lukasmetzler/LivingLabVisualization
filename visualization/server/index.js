f13b345c59c9ae60ca8430f7777cbd26a87cb2b7
Lukas Metzler <lukas.metzler@wheele.de>
2024-07-04 22:23:56 +0200

    swagger added etc.

--- visualization/server/index.js
+++ visualization/server/index.js
@@ -2,13 +2,14 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const authRoutes = require("./routes/auth");
+const { swaggerUi, specs } = require("./swagger");
 
 const app = express();
 const port = process.env.PORT || 5000;
 
 // CORS-Konfiguration
 const corsOptions = {
-  origin: "*", // Passen Sie dies an Ihre spezifische Domain an, z.B. "http://localhost:3000"
+  origin: "*",
   methods: ["GET", "POST", "PUT", "DELETE"],
   allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true,
@@ -19,6 +20,7 @@ app.use(cors(corsOptions));
 app.use(bodyParser.json());
 
 app.use("/auth", authRoutes);
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(specs));
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
---
+++ visualization/server/swagger.js
@@ -0,0 +1,26 @@
+const swaggerJsDoc = require("swagger-jsdoc");
+const swaggerUi = require("swagger-ui-express");
+
+const options = {
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "API Documentation",
+      version: "1.0.0",
+      description: "API documentation for your application",
+    },
+    servers: [
+      {
+        url: "http://localhost:5000",
+      },
+    ],
+  },
+  apis: ["./routes/*.js"], // Pfad zu den API-Routen
+};
+
+const specs = swaggerJsDoc(options);
+
+module.exports = {
+  swaggerUi,
+  specs,
+};
