8ce8f6d8e7423b7c3aa8c331e1e6c5fbd5be766d
Lukas Metzler <lukas.metzler@wheele.de>
2024-07-11 19:47:16 +0200

    producer.py edit

--- kafka/producer.py
+++ kafka/producer.py
@@ -5,16 +5,16 @@ import signal
 import sys
 from time import sleep
 from typing import Dict, List
-import psycopg2
 from kafka import KafkaProducer
 import config
 import column_names as cn
 
+
 def stop_producer(signum, frame):
     logging.info("Stopping producer...")
     producer.close()
-    db_connection.close()
     sys.exit(0)
+
 
 def generate_random_data(table_column_names: Dict[str, List[str]]) -> Dict[str, float]:
     data = {}
@@ -24,14 +24,6 @@ def generate_random_data(table_column_names: Dict[str, List[str]]) -> Dict[str, 
     logging.debug("Generated data: %s", data)
     return data
 
-def establish_db_connection(configurations):
-    return psycopg2.connect(
-        dbname=configurations.CONSUMER_POSTGRES_DB,
-        user=configurations.CONSUMER_POSTGRES_USER,
-        password=configurations.CONSUMER_POSTGRES_PASSWORD,
-        host=configurations.CONSUMER_POSTGRES_HOST,
-        port=configurations.CONSUMER_POSTGRES_PORT,
-    )
 
 def start_producer(configurations):
     return KafkaProducer(
@@ -39,13 +31,13 @@ def start_producer(configurations):
         value_serializer=lambda v: json.dumps(v).encode("utf-8"),
     )
 
+
 def main():
     configurations = config.load_config()
     logging.info("Starting the Producer...")
     logging.info(f"KAFKA_BOOTSTRAP_SERVER: {configurations.KAFKA_BOOTSTRAP_SERVER}")
 
-    global db_connection, producer
-    db_connection = establish_db_connection(configurations)
+    global producer
     producer = start_producer(configurations)
 
     kafka_topic = configurations.KAFKA_TOPIC
@@ -59,6 +51,7 @@ def main():
         logging.debug("Data for tables: %s", data_for_tables)
         producer.send(kafka_topic, value=data_for_tables)
         sleep(wait_between_iterations)
+
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO)
