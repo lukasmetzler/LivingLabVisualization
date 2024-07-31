d4f76f8f2f633131a0544e1f5d8c889200dd1f41
Lukas Metzler <lukas.metzler@wheele.de>
2024-07-31 19:38:27 +0200

    consumer.py edit

--- kafka/consumer.py
+++ kafka/consumer.py
@@ -166,7 +166,6 @@ def process_messages():
 if __name__ == "__main__":
     consumer = KafkaConsumer(
         c.KAFKA_TOPIC,
-        "zed_kamera_topic",
         bootstrap_servers=[c.KAFKA_BOOTSTRAP_SERVER],
         auto_offset_reset="earliest",
         enable_auto_commit=True,
