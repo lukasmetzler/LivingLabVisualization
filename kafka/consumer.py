35f961514ab634094a4031558798731935b966b6
Lukas Metzler <lukas.metzler@wheele.de>
2024-08-04 20:22:36 +0200

    alemibc and sqlalchemy migration

--- kafka/consumer.py
+++ kafka/consumer.py
@@ -2,14 +2,26 @@ import json
 import logging
 from kafka import KafkaConsumer
 import config
-import postgres as pg
-from column_names import table_column_names
+from models import (
+    get_engine,
+    Base,
+    DimZedBodyTracking1ogR1,
+    DimMetrologicalData,
+    DimPvModulData1ogR1,
+    DimIlluminationDatapoints1ogR1,
+    DimRaffstoreLightData,
+    DimUserInput,
+)
+from sqlalchemy.orm import sessionmaker
 import signal
 import sys
 
 c = config.load_config()
 logger = logging.getLogger(__name__)
-logger.setLevel(logging.DEBUG)  # Set log level to DEBUG to get more detailed logs
+logger.setLevel(logging.DEBUG)
+
+engine = get_engine()
+Session = sessionmaker(bind=engine)
 
 
 def stop_consumer(signum, frame):
@@ -18,157 +30,70 @@ def stop_consumer(signum, frame):
     sys.exit(0)
 
 
-def get_last_inserted_ids(cursor):
-    last_inserted_ids = {}
+def process_zed_kamera_data(session, data):
     try:
-        for table_name, column_names in table_column_names.items():
-            if table_name.startswith("dim_"):
-                query = f"SELECT * FROM {table_name} ORDER BY created_at DESC LIMIT 1"
-                cursor.execute(query)
-                result = cursor.fetchone()
-                if result:
-                    id_column_name = next(
-                        (
-                            col
-                            for col in column_names
-                            if col.endswith("_id") or col.endswith("_uuid")
-                        ),
-                        None,
-                    )
-                    if id_column_name:
-                        last_inserted_ids[id_column_name] = result[
-                            column_names.index(id_column_name)
-                        ]
-                    else:
-                        logger.error(f"No ID column found for table {table_name}")
-        return last_inserted_ids
+        zed_data = DimZedBodyTracking1ogR1(
+            is_new=data.get("is_new", False),
+            is_tracked=data.get("is_tracked", False),
+            camera_pitch=data.get("camera_pitch"),
+            camera_roll=data.get("camera_roll"),
+            camera_yaw=data.get("camera_yaw"),
+            body_list=data.get("body_list", []),
+        )
+        session.add(zed_data)
+        session.commit()
+        logger.info(f"Data inserted into dim_zed_body_tracking_1og_r1: {data}")
     except Exception as e:
-        logger.error(f"An error occurred while retrieving last inserted IDs: {e}")
-        return None
+        logger.error(f"An error occurred while inserting zed kamera data: {e}")
+        session.rollback()
 
 
-def insert_data_into_table(connection, cursor, table_name, column_names, data):
+def process_data(session, table_name, data):
     try:
-        if table_name.startswith("dim_") and isinstance(data, dict):
-            table_data = data.get(table_name, None)
-            if table_data is None:
-                logger.error(
-                    f"Table data for '{table_name}' not found in the message. Skipping message."
-                )
-                return None
-            filtered_values = [
-                table_data.get(column, None)
-                for column in column_names
-                if not column.endswith("_id") and column != "created_at"
-            ]
-            filtered_columns = [
-                column
-                for column in column_names
-                if not column.endswith("_id") and column != "created_at"
-            ]
-            columns_placeholder = ", ".join(filtered_columns)
-            values_placeholder = ", ".join(["%s" for _ in filtered_columns])
-            query = f"INSERT INTO {table_name} ({columns_placeholder}) VALUES ({values_placeholder})"
-            cursor.execute(query, filtered_values)
-            logger.info(f"Data inserted into {table_name}")
-            return None
+        if table_name == "dim_metrological_data":
+            table_data = DimMetrologicalData(**data)
+        elif table_name == "dim_pv_modul_data_1og_r1":
+            table_data = DimPvModulData1ogR1(**data)
+        elif table_name == "dim_illumination_datapoints_1og_r1":
+            table_data = DimIlluminationDatapoints1ogR1(**data)
+        elif table_name == "dim_raffstore_light_data":
+            table_data = DimRaffstoreLightData(**data)
+        elif table_name == "dim_user_input":
+            table_data = DimUserInput(**data)
+        else:
+            logger.error(f"Unknown table name: {table_name}")
+            return
+
+        session.add(table_data)
+        session.commit()
+        logger.info(f"Data inserted into {table_name}: {data}")
     except Exception as e:
         logger.error(f"An error occurred while inserting data into {table_name}: {e}")
-        connection.rollback()
-        return None
-
-
-def process_zed_kamera_data(connection, cursor, data):
-    try:
-        table_name = "dim_zed_body_tracking_1og_r1"
-        column_names = table_column_names[table_name]
-        filtered_values = [
-            data.get(column, None)
-            for column in column_names
-            if not column.endswith("_id") and column != "created_at"
-        ]
-        filtered_columns = [
-            column
-            for column in column_names
-            if not column.endswith("_id") and column != "created_at"
-        ]
-        columns_placeholder = ", ".join(filtered_columns)
-        values_placeholder = ", ".join(["%s" for _ in filtered_columns])
-        query = f"INSERT INTO {table_name} ({columns_placeholder}) VALUES ({values_placeholder})"
-        cursor.execute(query, filtered_values)
-        connection.commit()
-        logger.info(f"Data inserted into {table_name}")
-    except Exception as e:
-        logger.error(f"An error occurred while inserting data into {table_name}: {e}")
-        connection.rollback()
+        session.rollback()
 
 
 def process_messages():
+    session = Session()
     try:
-        with pg.postgres_connection() as connection:
-            logger.debug("Connection established successfully")
-            for message in consumer:
-                logger.debug(f"Received message: {message.value}")
-                with connection.cursor() as cursor:
-                    if message.topic == "zed_kamera_topic":
-                        process_zed_kamera_data(connection, cursor, message.value)
-                    else:
-                        dimension_ids = {}
-                        for table_name, column_names in table_column_names.items():
-                            inserted_id = insert_data_into_table(
-                                connection,
-                                cursor,
-                                table_name,
-                                column_names,
-                                message.value,
-                            )
-                            dimension_ids[table_name] = inserted_id
-                        last_inserted_ids = get_last_inserted_ids(cursor)
-                        if not last_inserted_ids:
-                            logger.error(
-                                "No last inserted IDs found. Skipping fact table insertion."
-                            )
-                            continue
-                        fact_tables = [
-                            table_name
-                            for table_name in table_column_names.keys()
-                            if table_name.startswith("fact_")
-                        ]
-                        for table_name in fact_tables:
-                            column_names = table_column_names[table_name]
-                            if column_names:
-                                column_names_str = ", ".join(column_names)
-                                placeholders = ", ".join(["%s" for _ in column_names])
-                                for column_name in column_names:
-                                    inserted_id = last_inserted_ids.get(column_name)
-                                    if inserted_id is None:
-                                        logger.warning(
-                                            f"ID for '{column_name}' not found."
-                                        )
-                                query = f"INSERT INTO {table_name} ({column_names_str}) VALUES ({placeholders})"
-                                cursor.execute(
-                                    query,
-                                    tuple(
-                                        last_inserted_ids[col] for col in column_names
-                                    ),
-                                )
-                            else:
-                                logger.warning(
-                                    f"Column names for table '{table_name}' not found."
-                                )
-                        connection.commit()
-                        logger.info("Dimension tables inserted successfully")
+        for message in consumer:
+            logger.debug(f"Received message: {message.value}")
+            data = message.value
+            if message.topic == "zed_kamera_topic":
+                process_zed_kamera_data(session, data)
+            else:
+                for table_name, table_data in data.items():
+                    process_data(session, table_name, table_data)
+        session.commit()
     except Exception as e:
         logger.error(f"An error occurred: {e}")
+        session.rollback()
     finally:
         consumer.close()
+        session.close()
 
 
 if __name__ == "__main__":
-    # Debugging: Überprüfen Sie die geladenen Themen
     print(f"Loaded Kafka Topics: {c.KAFKA_TOPICS}")
-
-    # Debugging: Überprüfen Sie den Bootstrap-Server
     print(f"Kafka Bootstrap Server: {c.KAFKA_BOOTSTRAP_SERVER}")
 
     if not c.KAFKA_TOPICS:
@@ -176,7 +101,7 @@ if __name__ == "__main__":
 
     try:
         consumer = KafkaConsumer(
-            *c.KAFKA_TOPICS,  # Entpacken der Liste von Themen
+            *c.KAFKA_TOPICS,
             bootstrap_servers=[c.KAFKA_BOOTSTRAP_SERVER],
             auto_offset_reset="earliest",
             enable_auto_commit=True,
---
+++ kafka/models.py
@@ -0,0 +1,252 @@
+from sqlalchemy import (
+    create_engine,
+    Column,
+    String,
+    Numeric,
+    Boolean,
+    JSON,
+    TIMESTAMP,
+    Integer,
+    ForeignKey,
+)
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.dialects.postgresql import UUID
+from sqlalchemy.orm import relationship
+
+Base = declarative_base()
+
+
+class DimMetrologicalData(Base):
+    __tablename__ = "dim_metrological_data"
+    metrological_data_id = Column(
+        UUID(as_uuid=True), primary_key=True, server_default="uuid_generate_v4()"
+    )
+    GlobIrrVerAct = Column(Numeric)
+    GlobalIrrHorAct = Column(Numeric)
+    DifflrrHorAct = Column(Numeric)
+    WindSpeedAct_ms = Column(Numeric)
+    SunElevationAct = Column(Numeric)
+    SunAzimuthAct = Column(Numeric)
+    Longitude = Column(Numeric)
+    Latitude = Column(Numeric)
+    WindSpeedAct_kmh = Column(Numeric)
+    WindDirectionAct = Column(Numeric)
+    BrightnessNorthAct = Column(Numeric)
+    BrightnessSouthAct = Column(Numeric)
+    BrightnessWestAct = Column(Numeric)
+    TwilightAct = Column(Numeric)
+    GlobalIrrHorAct_2 = Column(Numeric)
+    PrecipitationAct = Column(Numeric)
+    AbsolutAirPressureAct = Column(Numeric)
+    RelativeAirPressureAct = Column(Numeric)
+    AbsoluteHumidityAct = Column(Numeric)
+    RelativeHumidityAct = Column(Numeric)
+    DewPointTempAct = Column(Numeric)
+    HousingTemAct = Column(Numeric)
+    RoomTempAct = Column(Numeric)
+    created_at = Column(TIMESTAMP, server_default="CURRENT_TIMESTAMP")
+
+
+class DimZedBodyTracking1ogR1(Base):
+    __tablename__ = "dim_zed_body_tracking_1og_r1"
+    zed_body_tracking_id = Column(
+        UUID(as_uuid=True), primary_key=True, server_default="uuid_generate_v4()"
+    )
+    is_new = Column(Boolean, default=False)
+    is_tracked = Column(Boolean, default=False)
+    camera_pitch = Column(Numeric)
+    camera_roll = Column(Numeric)
+    camera_yaw = Column(Numeric)
+    body_list = Column(JSON, default="[]")
+    created_at = Column(TIMESTAMP, server_default="CURRENT_TIMESTAMP")
+
+
+class DimPvModulData1ogR1(Base):
+    __tablename__ = "dim_pv_modul_data_1og_r1"
+    pv_modul_data_id = Column(
+        UUID(as_uuid=True), primary_key=True, server_default="uuid_generate_v4()"
+    )
+    volt_meas_act_module1 = Column(Numeric)
+    curr_meas_act_module1 = Column(Numeric)
+    volt_meas_act_module2 = Column(Numeric)
+    curr_meas_act_module2 = Column(Numeric)
+    created_at = Column(TIMESTAMP, server_default="CURRENT_TIMESTAMP")
+
+
+class DimIlluminationDatapoints1ogR1(Base):
+    __tablename__ = "dim_illumination_datapoints_1og_r1"
+    illumination_datapoints_id = Column(
+        UUID(as_uuid=True), primary_key=True, server_default="uuid_generate_v4()"
+    )
+    illum_mp1_act = Column(Numeric)
+    illum_mp2_act = Column(Numeric)
+    illum_mp3_act = Column(Numeric)
+    illum_mp4_act = Column(Numeric)
+    created_at = Column(TIMESTAMP, server_default="CURRENT_TIMESTAMP")
+
+
+class DimRaffstoreLightData(Base):
+    __tablename__ = "dim_raffstore_light_data"
+    raffstore_light_data_id = Column(
+        UUID(as_uuid=True), primary_key=True, server_default="uuid_generate_v4()"
+    )
+    room_number = Column(Integer)
+    slat_ang1_act = Column(Numeric)
+    slat_pos1_act = Column(Numeric)
+    slat_ang2_act = Column(Numeric)
+    slat_pos2_act = Column(Numeric)
+    slat_ang3_act = Column(Numeric)
+    slat_pos3_act = Column(Numeric)
+    light1_act = Column(Numeric)
+    light2_act = Column(Numeric)
+    created_at = Column(TIMESTAMP, server_default="CURRENT_TIMESTAMP")
+
+
+class DimUserInput(Base):
+    __tablename__ = "dim_user_input"
+    user_input_id = Column(
+        UUID(as_uuid=True), primary_key=True, server_default="uuid_generate_v4()"
+    )
+    mp_glare_limit = Column(Numeric)
+    mp_req_illum = Column(Numeric)
+    mp_req_room_temp = Column(Numeric)
+    measurement_point = Column(Integer)
+    created_at = Column(TIMESTAMP, server_default="CURRENT_TIMESTAMP")
+
+
+class DimTime(Base):
+    __tablename__ = "dim_time"
+    time_id = Column(
+        UUID(as_uuid=True), primary_key=True, server_default="uuid_generate_v4()"
+    )
+    timestamp = Column(TIMESTAMP)
+    date = Column(String)
+    day_of_week = Column(String)
+    month = Column(String)
+    quarter = Column(String)
+    year = Column(Integer)
+    hour = Column(Integer)
+    created_at = Column(TIMESTAMP, server_default="CURRENT_TIMESTAMP")
+
+
+class DimLocation(Base):
+    __tablename__ = "dim_location"
+    location_id = Column(
+        UUID(as_uuid=True), primary_key=True, server_default="uuid_generate_v4()"
+    )
+    room_number = Column(Integer)
+    building = Column(String)
+    created_at = Column(TIMESTAMP, server_default="CURRENT_TIMESTAMP")
+
+
+class FactUserInputFacts(Base):
+    __tablename__ = "fact_user_input_facts"
+    user_input_facts_id = Column(
+        UUID(as_uuid=True), primary_key=True, server_default="uuid_generate_v4()"
+    )
+    time_id = Column(UUID(as_uuid=True), ForeignKey("dim_time.time_id"))
+    location_id = Column(UUID(as_uuid=True), ForeignKey("dim_location.location_id"))
+    user_input_id = Column(
+        UUID(as_uuid=True), ForeignKey("dim_user_input.user_input_id")
+    )
+
+
+class FactSensory(Base):
+    __tablename__ = "fact_sensory"
+    sensory_id = Column(
+        UUID(as_uuid=True), primary_key=True, server_default="uuid_generate_v4()"
+    )
+    time_id = Column(UUID(as_uuid=True), ForeignKey("dim_time.time_id"))
+    location_id = Column(UUID(as_uuid=True), ForeignKey("dim_location.location_id"))
+    zed_body_tracking_id = Column(
+        UUID(as_uuid=True),
+        ForeignKey("dim_zed_body_tracking_1og_r1.zed_body_tracking_id"),
+    )
+
+
+class FactRaffstoreLightFacts(Base):
+    __tablename__ = "fact_raffstore_light_facts"
+    raffstore_light_facts_id = Column(
+        UUID(as_uuid=True), primary_key=True, server_default="uuid_generate_v4()"
+    )
+    time_id = Column(UUID(as_uuid=True), ForeignKey("dim_time.time_id"))
+    location_id = Column(UUID(as_uuid=True), ForeignKey("dim_location.location_id"))
+    raffstore_light_data_id = Column(
+        UUID(as_uuid=True),
+        ForeignKey("dim_raffstore_light_data.raffstore_light_data_id"),
+    )
+
+
+class FactEnvironmentalDataFacts(Base):
+    __tablename__ = "fact_environmental_data_facts"
+    environmental_data_facts_id = Column(
+        UUID(as_uuid=True), primary_key=True, server_default="uuid_generate_v4()"
+    )
+    time_id = Column(UUID(as_uuid=True), ForeignKey("dim_time.time_id"))
+    location_id = Column(UUID(as_uuid=True), ForeignKey("dim_location.location_id"))
+    metrological_data_id = Column(
+        UUID(as_uuid=True), ForeignKey("dim_metrological_data.metrological_data_id")
+    )
+    pv_modul_data_id = Column(
+        UUID(as_uuid=True), ForeignKey("dim_pv_modul_data_1og_r1.pv_modul_data_id")
+    )
+    illumination_datapoints_id = Column(
+        UUID(as_uuid=True),
+        ForeignKey("dim_illumination_datapoints_1og_r1.illumination_datapoints_id"),
+    )
+    radiation_forecast_id = Column(
+        UUID(as_uuid=True), ForeignKey("dim_radiation_forecast.radiation_forecast_id")
+    )
+    head_positions_id = Column(
+        UUID(as_uuid=True), ForeignKey("dim_head_positions_1og_r1.head_positions_id")
+    )
+
+
+class DimRadiationForecast(Base):
+    __tablename__ = "dim_radiation_forecast"
+    radiation_forecast_id = Column(
+        UUID(as_uuid=True), primary_key=True, server_default="uuid_generate_v4()"
+    )
+    global_irr_hor_approx = Column(Numeric)
+    diff_irr_hor_act_approx = Column(Numeric)
+    created_at = Column(TIMESTAMP, server_default="CURRENT_TIMESTAMP")
+
+
+class DimHeadPositions1ogR1(Base):
+    __tablename__ = "dim_head_positions_1og_r1"
+    head_positions_id = Column(
+        UUID(as_uuid=True), primary_key=True, server_default="uuid_generate_v4()"
+    )
+    headpose_x_1 = Column(Numeric)
+    headpose_y_1 = Column(Numeric)
+    headpose_z_1 = Column(Numeric)
+    headpose_pitch_1 = Column(Numeric)
+    headpose_yaw_1 = Column(Numeric)
+    headpose_roll_1 = Column(Numeric)
+    headpose_x_2 = Column(Numeric)
+    headpose_y_2 = Column(Numeric)
+    headpose_z_2 = Column(Numeric)
+    headpose_pitch_2 = Column(Numeric)
+    headpose_yaw_2 = Column(Numeric)
+    headpose_roll_2 = Column(Numeric)
+    headpose_x_3 = Column(Numeric)
+    headpose_y_3 = Column(Numeric)
+    headpose_z_3 = Column(Numeric)
+    headpose_pitch_3 = Column(Numeric)
+    headpose_yaw_3 = Column(Numeric)
+    headpose_roll_3 = Column(Numeric)
+    headpose_x_4 = Column(Numeric)
+    headpose_y_4 = Column(Numeric)
+    headpose_z_4 = Column(Numeric)
+    headpose_pitch_4 = Column(Numeric)
+    headpose_yaw_4 = Column(Numeric)
+    headpose_roll_4 = Column(Numeric)
+    created_at = Column(TIMESTAMP, server_default="CURRENT_TIMESTAMP")
+
+
+def get_engine():
+    from config import load_config
+
+    c = load_config()
+    db_url = f"postgresql+psycopg2://{c.CONSUMER_POSTGRES_USER}:{c.CONSUMER_POSTGRES_PASSWORD}@{c.CONSUMER_POSTGRES_HOST}:{c.CONSUMER_POSTGRES_PORT}/{c.CONSUMER_POSTGRES_DB}"
+    return create_engine(db_url)
