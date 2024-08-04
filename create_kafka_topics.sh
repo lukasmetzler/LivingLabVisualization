509d108c808961e7d86028add360ef3ae69e048e
Lukas Metzler <lukas.metzler@wheele.de>
2024-08-04 21:04:47 +0200

    create_kafka_topics edit

--- create_kafka_topics.sh
+++ create_kafka_topics.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-# Wait for Kafka to start
+# Warten, bis Kafka gestartet ist
 sleep 10
 
-# Create Kafka topic
-/usr/bin/kafka-topics.sh --create --topic hella_data_topic --bootstrap-server kafka_new:9092 --replication-factor 1 --partitions 1
-/usr/bin/kafka-topics.sh --create --topic zed_kamera_topic --bootstrap-server kafka_new:9092 --replication-factor 1 --partitions 1
+# Kafka-Themen erstellen
+docker exec -it kafka_new kafka-topics --create --topic hella_data_topic --bootstrap-server kafka_new:9092 --partitions 1 --replication-factor 1
+docker exec -it kafka_new kafka-topics --create --topic zed_kamera_topic --bootstrap-server kafka_new:9092 --partitions 1 --replication-factor 1
