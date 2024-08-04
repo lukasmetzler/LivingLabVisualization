c12d51aefc1c4c44dca3753d8bae807142ab8cf2
Lukas Metzler <lukas.metzler@wheele.de>
2024-08-04 21:10:39 +0200

    init-db.sh edit

--- db/init-db.sh
+++ db/init-db.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-docker exec -i postgres_new psql -U "$POSTGRES_USER" <<-EOSQL
+docker exec -i postgres_new psql -U "$POSTGRES_USER" -d postgres <<-EOSQL
     CREATE DATABASE LivingLabVisualization;
     CREATE USER lukasmetzler WITH ENCRYPTED PASSWORD 'lukasmetzler';
     GRANT ALL PRIVILEGES ON DATABASE LivingLabVisualization TO lukasmetzler;
