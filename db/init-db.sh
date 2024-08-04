ae8e9d08673a4a2acdd0425e5141017622e017c4
Lukas Metzler <lukas.metzler@wheele.de>
2024-08-04 20:33:23 +0200

    refactoring of the whole project

--- db/init-db.sh
+++ db/init-db.sh
@@ -2,7 +2,7 @@
 set -e
 
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
-    CREATE DATABASE evi;
+    CREATE DATABASE LivingLabVisualization;
     CREATE USER lukasmetzler WITH ENCRYPTED PASSWORD 'lukasmetzler';
     GRANT ALL PRIVILEGES ON DATABASE evi TO lukasmetzler;
 EOSQL
