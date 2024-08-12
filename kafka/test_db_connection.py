13e66b05a21a4b10b24ed44a86955e52246d6206
Lukas Metzler <lukas.metzler@wheele.de>
2024-08-12 22:21:46 +0200

    test_db_connection

---
+++ kafka/test_db_connection.py
@@ -0,0 +1,17 @@
+import psycopg2
+
+conn = psycopg2.connect(
+    dbname="livinglabvisualization",
+    user="lukasmetzler",
+    password="lukasmetzler",
+    host="postgres_new",
+    port="5432",
+)
+
+cur = conn.cursor()
+cur.execute(
+    "SELECT table_name FROM information_schema.tables WHERE table_schema='public';"
+)
+print(cur.fetchall())
+cur.close()
+conn.close()
