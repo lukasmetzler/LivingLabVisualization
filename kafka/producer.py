7ff673626721ec915807aeb46569846106c4d036
Lukas Metzler <lukas.metzler@wheele.de>
2024-09-01 18:11:53 +0200

    producer.py test for topic seperation

--- kafka/producer.py
+++ kafka/producer.py
@@ -16,7 +16,9 @@ def stop_producer(signum, frame):
     sys.exit(0)
 
 
-def generate_random_data(table_column_names: Dict[str, List[str]]) -> Dict[str, float]:
+def generate_random_data(
+    table_column_names: Dict[str, List[str]]
+) -> Dict[str, Dict[str, float]]:
     data = {}
     for table, columns in table_column_names.items():
         filtered_columns = [column for column in columns if not column.endswith("_id")]
@@ -40,7 +42,7 @@ def main():
     global producer
     producer = start_producer(configurations)
 
-    kafka_topics = configurations.KAFKA_TOPICS
+    kafka_topics = configurations.KAFKA_TOPICS  # Verwenden Sie KAFKA_TOPICS
     wait_between_iterations = configurations.PRODUCER_INTERVAL_SECONDS
     logging.info("Starting the producer loop...")
 
@@ -49,9 +51,23 @@ def main():
     while True:
         data_for_tables = generate_random_data(cn.table_column_names)
         logging.debug("Data for tables: %s", data_for_tables)
-        for topic in kafka_topics:
-            producer.send(topic, value=data_for_tables[topic])
-            logging.info(f"Sent data to topic {topic}: {data_for_tables[topic]}")
+
+        # Beispielhafte Zuweisung von Daten zu Topics
+        hella_data = {
+            key: value
+            for key, value in data_for_tables.items()
+            if key != "dim_zed_body_tracking_1og_r1"
+        }
+        zed_data = data_for_tables.get("dim_zed_body_tracking_1og_r1", {})
+
+        if "hella_data_topic" in kafka_topics:
+            producer.send("hella_data_topic", value=hella_data)
+            logging.info(f"Sent data to topic hella_data_topic: {hella_data}")
+
+        if "zed_kamera_topic" in kafka_topics:
+            producer.send("zed_kamera_topic", value=zed_data)
+            logging.info(f"Sent data to topic zed_kamera_topic: {zed_data}")
+
         sleep(wait_between_iterations)
 
 
