41e8c14f911fbeb282ed72e5bbdc930daf7e4d0e
Lukas Metzler <lukas.metzler@wheele.de>
2024-09-09 19:21:02 +0200

    consumer.py edit

--- kafka/consumer.py
+++ kafka/consumer.py
@@ -30,8 +30,8 @@ logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
 Session = sessionmaker(bind=engine)
 
-# Zuordnung von Kafka Tabellennamen zu Klassen
-model_mapping = {
+# Table class mapping
+table_to_class = {
     "dim_zed_body_tracking_1og_r1": DimZedBodyTracking1ogR1,
     "dim_metrological_data": DimMetrologicalData,
     "dim_pv_modul_data_1og_r1": DimPvModulData1ogR1,
@@ -56,17 +56,16 @@ def stop_consumer(signum, frame):
 
 def process_data(session, table_name, data):
     try:
-        model = model_mapping.get(table_name)
-        if model is None:
+        model = table_to_class.get(table_name)
+        if not model:
             logger.error(f"Unknown table name: {table_name}")
             return
 
-        if table_name == "dim_metrological_data":
-            timestamp = data.get("created_at")
-            if timestamp:
-                timestamp = datetime.datetime.fromisoformat(timestamp)
-                time_record = DimTime.get_or_create(session, timestamp)
-                data["time_id"] = time_record.time_id
+        # Special handling for metrological data
+        if table_name == "dim_metrological_data" and "created_at" in data:
+            timestamp = datetime.datetime.fromisoformat(data["created_at"])
+            time_record = DimTime.get_or_create(session, timestamp)
+            data["time_id"] = time_record.time_id
 
         table_data = model(**data)
         session.add(table_data)
@@ -82,7 +81,7 @@ def process_messages():
     try:
         for message in consumer:
             logger.debug(f"Received message: {message.value}")
-            data = message.value
+            data = json.loads(message.value)
             if message.topic == "zed_kamera_topic":
                 process_zed_kamera_data(session, data)
             else:
@@ -102,17 +101,18 @@ if __name__ == "__main__":
     print(f"Kafka Bootstrap Server: {c.KAFKA_BOOTSTRAP_SERVER}")
     if not c.KAFKA_TOPICS:
         raise ValueError("No Kafka topics found. Please check your configuration.")
+
     try:
         consumer = KafkaConsumer(
             *c.KAFKA_TOPICS,
-            bootstrap_servers=[c.KAFKA_BOOTSTRAP_SERVER],
+            bootstrap_servers=c.KAFKA_BOOTSTRAP_SERVER,
             auto_offset_reset="earliest",
             enable_auto_commit=True,
             group_id="consumer",
             value_deserializer=lambda x: json.loads(x.decode("utf-8")),
         )
+        signal.signal(signal.SIGINT, stop_consumer)
+        process_messages()
     except Exception as e:
         logger.error(f"An error occurred while creating KafkaConsumer: {e}")
         sys.exit(1)
-    signal.signal(signal.SIGINT, stop_consumer)
-    process_messages()
