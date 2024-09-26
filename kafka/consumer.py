ebfcee2ad417090995fd90faa951a406bb6cefbe
MetzlerL <lukas.metzler@compax.at>
2024-09-26 10:39:41 +0200

    test

--- kafka/consumer.py
+++ kafka/consumer.py
@@ -29,6 +29,7 @@ c = config.load_config()
 
 # Logging konfigurieren, um Nachrichten zu debuggen und Fehler zu verfolgen
 logger = logging.getLogger(__name__)
+logging.basicConfig(level=logging.DEBUG)
 logger.setLevel(logging.DEBUG)
 
 # Sessionmaker für die Datenbank-Sitzung initialisieren
