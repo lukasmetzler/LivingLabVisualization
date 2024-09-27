b347c3ec7eeec24ea936c219c5e20143b0a15f0a
Lukas Metzler <lukas.metzler@wheele.de>
2024-09-27 22:04:53 +0200

    test

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,10 +1,12 @@
+// ecosystem.config.js
+
 module.exports = {
   apps: [
     {
       name: "frontend",
       cwd: "frontend",
       script: "npm",
-      args: "run start", // Passe dies an, falls dein Start-Skript anders heißt
+      args: "run start",
       watch: false,
       env: {
         NODE_ENV: "production",
@@ -14,15 +16,15 @@ module.exports = {
       name: "backend",
       cwd: "backend",
       script: "npm",
-      args: "run start", // Passe dies an, falls dein Start-Skript anders heißt
+      args: "run start",
       watch: false,
       env: {
         NODE_ENV: "production",
         JWT_SECRET: process.env.JWT_SECRET || "test",
         DB_HOST: "postgres_new",
-        DB_USER: process.env.POSTGRES_USER,
-        DB_PASSWORD: process.env.POSTGRES_PASSWORD,
-        DB_NAME: process.env.POSTGRES_DB,
+        DB_USER: process.env.DB_USER,
+        DB_PASSWORD: process.env.DB_PASSWORD,
+        DB_NAME: process.env.DB_NAME,
         DB_PORT: "5432",
       },
     },
