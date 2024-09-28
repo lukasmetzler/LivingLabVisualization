11b68ea4bcaf23193255ce6660775617909d2f13
Lukas Metzler <lukas.metzler@wheele.de>
2024-09-28 19:02:08 +0200

    test

--- kafka/create_tables.py
+++ kafka/create_tables.py
@@ -1,8 +1,29 @@
 # create_tables.py
 
+import time
+import os
+from sqlalchemy import create_engine
+from sqlalchemy.exc import OperationalError
 from alembic import command
 from alembic.config import Config
-import os
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+# Warte auf die Datenbankverbindung
+print("Warte auf PostgreSQL...")
+for i in range(30):
+    try:
+        engine = create_engine(DATABASE_URL)
+        connection = engine.connect()
+        connection.close()
+        print("PostgreSQL ist verfügbar")
+        break
+    except OperationalError:
+        print("PostgreSQL ist noch nicht verfügbar, warte...")
+        time.sleep(1)
+else:
+    print("Fehler: Konnte keine Verbindung zur Datenbank herstellen.")
+    exit(1)
 
 # Lade die Alembic-Konfiguration
 alembic_cfg = Config(os.path.join(os.path.dirname(__file__), "alembic.ini"))
