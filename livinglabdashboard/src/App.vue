c7b662aa4513a12b71ec5a693ff590605cd3a157
Lukas Metzler <lukas.metzler@wheele.de>
2024-09-29 22:05:27 +0200

    App.vue

--- livinglabdashboard/src/App.vue
+++ livinglabdashboard/src/App.vue
@@ -1,30 +1,50 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <v-app>
+    <v-app-bar app>
+      <v-img
+        src="/vite.svg"
+        alt="Vite Logo"
+        max-width="50"
+        class="mr-2"
+      ></v-img>
+      <v-img
+        src="./assets/vue.svg"
+        alt="Vue Logo"
+        max-width="50"
+        class="mr-4"
+      ></v-img>
+      <v-toolbar-title>LivingLab Dashboard</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-btn text to="/">Home</v-btn>
+      <v-btn text to="/login">Login</v-btn>
+      <v-btn text to="/dashboard">Dashboard</v-btn>
+      <v-btn text @click="logout" v-if="isAuthenticated">Logout</v-btn>
+    </v-app-bar>
+
+    <v-main>
+      <v-container>
+        <router-view></router-view>
+      </v-container>
+    </v-main>
+  </v-app>
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+<script setup>
+import { useAuthStore } from "./stores/auth";
+import { useRouter } from "vue-router";
+import { computed } from "vue";
+
+const authStore = useAuthStore();
+const router = useRouter();
+
+const logout = () => {
+  authStore.logout();
+  router.push("/login");
+};
+
+const isAuthenticated = computed(() => !!authStore.token);
+</script>
+
+<style>
+/* Optional globale Stile */
 </style>
