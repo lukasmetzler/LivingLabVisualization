3a59fa157a6d4a1a8ef18dd540c46e8b42aec5f7
Lukas Metzler <lukas.metzler@wheele.de>
2024-09-29 21:52:40 +0200

    test

---
+++ livinglabdashboard/src/views/Home.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-container>
+    <v-row>
+      <v-col>
+        <h1>Home</h1>
+        <p>Willkommen auf der Startseite des LivingLab Dashboards!</p>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: "Home",
+};
+</script>
+
+<style scoped>
+/* Optional: Styling */
+</style>
