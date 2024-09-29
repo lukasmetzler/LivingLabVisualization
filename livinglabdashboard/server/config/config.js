b08262a50a5cb92fd45757d0671c30fa5aa1c2f7
Lukas Metzler <lukas.metzler@wheele.de>
2024-09-29 21:14:12 +0200

    edit

--- livinglabdashboard/server/config/config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  development: {
-    username: "lukasmetzler",
-    password: "lukasmetzler",
-    database: "livinglabvisualization",
-    host: "localhost",
-    dialect: "postgres",
-  },
-};
